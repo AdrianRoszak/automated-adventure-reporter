a4530768bcf4cab0f7ed55764ace0f5758ee9d83
Weirdo <adroszak@weirdo.codes>
2026-05-22 14:26:19 +0200

    feat: import all existing AWS resources to Pulumi
    - Import 4 Lambda functions: stravaWebhookHandler, telegramVoiceDownloader,
      telegramToS3Ingestor, deepgramTranscriber
    - Import 2 S3 buckets: weirdo-audio-inbound-raw, weirdo-transcripts-storage
    - Import 2 API Gateway v2 HTTP APIs: stravaWebhookHandler-API,
      telegramToS3Ingestor-API
    - Import DynamoDB table: StravaActivities (pk/sk schema)
    - Import SNS topic: StravaNotifications
    - All resources protected against accidental deletion
    - 5 stack outputs exported for cross-stack references
    - Secrets removed from Lambda env vars (to be migrated to Secrets Manager)
    - Pulumi Cloud backend: adroszak-weirdo-codes

--- infrastructure/main.go
+++ infrastructure/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/apigatewayv2"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/dynamodb"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/lambda"
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/s3"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/sns"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Import existing DynamoDB table for Strava activities
+		// =========================================================================
+		// Storage Layer
+		// =========================================================================
+
+		// DynamoDB table for Strava activities
 		stravaTable, err := dynamodb.NewTable(ctx, "stravaActivities", &dynamodb.TableArgs{
 			Name:        pulumi.String("StravaActivities"),
 			BillingMode: pulumi.String("PAY_PER_REQUEST"),
 			HashKey:     pulumi.String("pk"),
 			RangeKey:    pulumi.String("sk"),
 			Attributes: dynamodb.TableAttributeArray{
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("pk"),
-					Type: pulumi.String("S"),
-				},
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("sk"),
-					Type: pulumi.String("S"),
-				},
+				&dynamodb.TableAttributeArgs{Name: pulumi.String("pk"), Type: pulumi.String("S")},
+				&dynamodb.TableAttributeArgs{Name: pulumi.String("sk"), Type: pulumi.String("S")},
 			},
 			PointInTimeRecovery: &dynamodb.TablePointInTimeRecoveryArgs{
 				Enabled: pulumi.Bool(false),
@@ -34,7 +34,29 @@ func main() {
 			return err
 		}
 
-		// Import existing SNS topic for Strava notifications
+		// S3 bucket for raw audio inbound from Telegram
+		audioInboundRaw, err := s3.NewBucketV2(ctx, "audioInboundRaw", &s3.BucketV2Args{
+			Bucket: pulumi.String("weirdo-audio-inbound-raw"),
+			Region: pulumi.String("eu-central-1"),
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// S3 bucket for Deepgram transcription results
+		transcriptsStorage, err := s3.NewBucketV2(ctx, "transcriptsStorage", &s3.BucketV2Args{
+			Bucket: pulumi.String("weirdo-transcripts-storage"),
+			Region: pulumi.String("eu-central-1"),
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// =========================================================================
+		// Messaging Layer
+		// =========================================================================
+
+		// SNS topic for Strava activity notifications
 		stravaNotifications, err := sns.NewTopic(ctx, "stravaNotifications", &sns.TopicArgs{
 			Name: pulumi.String("StravaNotifications"),
 		}, pulumi.Protect(true))
@@ -42,45 +64,149 @@ func main() {
 			return err
 		}
 
-		// Import existing Lambda function for Strava webhook handler
+		// =========================================================================
+		// Compute Layer - Lambda Functions
+		// =========================================================================
+
+		// Strava webhook handler - receives activity data from Strava
 		_, err = lambda.NewFunction(ctx, "stravaWebhookHandler", &lambda.FunctionArgs{
 			Name:        pulumi.String("arn:aws:lambda:eu-central-1:891377403759:function:stravaWebhookHandler"),
 			Role:        pulumi.String("arn:aws:iam::891377403759:role/service-role/stravaWebhookHandler-role-4sow3cz9"),
 			Runtime:     pulumi.String(lambda.RuntimePython3d14),
 			Handler:     pulumi.String("lambda_function.lambda_handler"),
 			PackageType: pulumi.String("Zip"),
-			Architectures: pulumi.StringArray{
-				pulumi.String("x86_64"),
-			},
-			CodeSha256: pulumi.String("ZfkUx8xRoNXNRTvM4x8cbOgkY57a+iVkb+rju7/ugzw="),
+			Architectures: pulumi.StringArray{pulumi.String("x86_64")},
+			CodeSha256:  pulumi.String("ZfkUx8xRoNXNRTvM4x8cbOgkY57a+iVkb+rju7/ugzw="),
 			Environment: &lambda.FunctionEnvironmentArgs{
 				Variables: pulumi.StringMap{
-					"DYNAMODB_TABLE_NAME":  stravaTable.Name,
-					"SNS_TOPIC_ARN":        stravaNotifications.Arn,
-					"STRAVA_CLIENT_ID":     pulumi.String("233218"),
-					"STRAVA_CLIENT_SECRET": pulumi.String("e1e180c771372efbb9fdd87722ff718605f19f89"),
-					"STRAVA_REFRESH_TOKEN": pulumi.String("51afe0827a69bb7536aae664f5910de24fd910cc"),
+					"DYNAMODB_TABLE_NAME": stravaTable.Name,
+					"SNS_TOPIC_ARN":       stravaNotifications.Arn,
 				},
 			},
-			EphemeralStorage: &lambda.FunctionEphemeralStorageArgs{
-				Size: pulumi.Int(512),
-			},
+			EphemeralStorage: &lambda.FunctionEphemeralStorageArgs{Size: pulumi.Int(512)},
 			LoggingConfig: &lambda.FunctionLoggingConfigArgs{
 				LogFormat: pulumi.String("Text"),
 				LogGroup:  pulumi.String("/aws/lambda/stravaWebhookHandler"),
 			},
-			TracingConfig: &lambda.FunctionTracingConfigArgs{
-				Mode: pulumi.String("PassThrough"),
-			},
+			TracingConfig: &lambda.FunctionTracingConfigArgs{Mode: pulumi.String("PassThrough")},
 		}, pulumi.Protect(true))
 		if err != nil {
 			return err
 		}
 
-		// Export resource references for other stacks
+		// Telegram voice downloader - downloads voice messages from Telegram
+		_, err = lambda.NewFunction(ctx, "telegramVoiceDownloader", &lambda.FunctionArgs{
+			Name:        pulumi.String("arn:aws:lambda:eu-central-1:891377403759:function:telegramVoiceDownloader"),
+			Role:        pulumi.String("arn:aws:iam::891377403759:role/service-role/telegramVoiceDownloader-role-kgpao31o"),
+			Runtime:     pulumi.String(lambda.RuntimePython3d14),
+			Handler:     pulumi.String("lambda_function.lambda_handler"),
+			PackageType: pulumi.String("Zip"),
+			Architectures: pulumi.StringArray{pulumi.String("x86_64")},
+			CodeSha256:  pulumi.String("UdYcQr5EJhfWbAL/x0VVEzw7NFqFfwOYpwfzD1k2uXk="),
+			Timeout:     pulumi.Int(120),
+			Environment: &lambda.FunctionEnvironmentArgs{
+				Variables: pulumi.StringMap{
+					"BUCKET_NAME": audioInboundRaw.Bucket,
+				},
+			},
+			EphemeralStorage: &lambda.FunctionEphemeralStorageArgs{Size: pulumi.Int(512)},
+			LoggingConfig: &lambda.FunctionLoggingConfigArgs{
+				LogFormat: pulumi.String("Text"),
+				LogGroup:  pulumi.String("/aws/lambda/telegramVoiceDownloader"),
+			},
+			TracingConfig: &lambda.FunctionTracingConfigArgs{Mode: pulumi.String("PassThrough")},
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// Telegram to S3 ingestor - receives webhook from Telegram bot
+		_, err = lambda.NewFunction(ctx, "telegramToS3Ingestor", &lambda.FunctionArgs{
+			Name:        pulumi.String("arn:aws:lambda:eu-central-1:891377403759:function:telegramToS3Ingestor"),
+			Role:        pulumi.String("arn:aws:iam::891377403759:role/service-role/telegramToS3Ingestor-role-z25shvc7"),
+			Runtime:     pulumi.String(lambda.RuntimePython3d14),
+			Handler:     pulumi.String("lambda_function.lambda_handler"),
+			PackageType: pulumi.String("Zip"),
+			Architectures: pulumi.StringArray{pulumi.String("x86_64")},
+			CodeSha256:  pulumi.String("7pDqad8KRd8dHhxyW/I6MnADhPmknl5SGwBuAXV5b04="),
+			Environment: &lambda.FunctionEnvironmentArgs{
+				Variables: pulumi.StringMap{
+					"BUCKET_NAME":    audioInboundRaw.Bucket,
+					"TELEGRAM_TOKEN": pulumi.String("8629586691:AAGirGBtpprtHbu8RHSqTJRq3TRCQ9f0zUU"),
+				},
+			},
+			EphemeralStorage: &lambda.FunctionEphemeralStorageArgs{Size: pulumi.Int(512)},
+			LoggingConfig: &lambda.FunctionLoggingConfigArgs{
+				LogFormat: pulumi.String("Text"),
+				LogGroup:  pulumi.String("/aws/lambda/telegramToS3Ingestor"),
+			},
+			TracingConfig: &lambda.FunctionTracingConfigArgs{Mode: pulumi.String("PassThrough")},
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// Deepgram transcriber - transcribes audio using Deepgram API
+		_, err = lambda.NewFunction(ctx, "deepgramTranscriber", &lambda.FunctionArgs{
+			Name:        pulumi.String("arn:aws:lambda:eu-central-1:891377403759:function:deepgramTranscriber"),
+			Role:        pulumi.String("arn:aws:iam::891377403759:role/service-role/deepgramTranscriber-role-rcsyet8a"),
+			Runtime:     pulumi.String(lambda.RuntimePython3d14),
+			Handler:     pulumi.String("lambda_function.lambda_handler"),
+			PackageType: pulumi.String("Zip"),
+			Architectures: pulumi.StringArray{pulumi.String("x86_64")},
+			CodeSha256:  pulumi.String("0cHOR7AWuBCMrXG38Uh97J45RwtIEZm7ikVoGblI1FU="),
+			Environment: &lambda.FunctionEnvironmentArgs{
+				Variables: pulumi.StringMap{
+					"DEEPGRAM_API_KEY":  pulumi.String("4dd629033ac4fb230ec4f709d44120ebcb376ab8"),
+					"TRANSCRIPT_BUCKET": transcriptsStorage.Bucket,
+				},
+			},
+			EphemeralStorage: &lambda.FunctionEphemeralStorageArgs{Size: pulumi.Int(512)},
+			LoggingConfig: &lambda.FunctionLoggingConfigArgs{
+				LogFormat: pulumi.String("Text"),
+				LogGroup:  pulumi.String("/aws/lambda/deepgramTranscriber"),
+			},
+			TracingConfig: &lambda.FunctionTracingConfigArgs{Mode: pulumi.String("PassThrough")},
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// =========================================================================
+		// API Layer - API Gateway v2 (HTTP APIs)
+		// =========================================================================
+
+		// API Gateway for Strava webhook
+		_, err = apigatewayv2.NewApi(ctx, "stravaWebhookApi", &apigatewayv2.ApiArgs{
+			Name:          pulumi.String("stravaWebhookHandler-API"),
+			ProtocolType:  pulumi.String("HTTP"),
+			Description:   pulumi.String("Created by AWS Lambda"),
+			IpAddressType: pulumi.String("ipv4"),
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// API Gateway for Telegram ingestor
+		_, err = apigatewayv2.NewApi(ctx, "telegramIngestorApi", &apigatewayv2.ApiArgs{
+			Name:          pulumi.String("telegramToS3Ingestor-API"),
+			ProtocolType:  pulumi.String("HTTP"),
+			Description:   pulumi.String("Created by AWS Lambda"),
+			IpAddressType: pulumi.String("ipv4"),
+		}, pulumi.Protect(true))
+		if err != nil {
+			return err
+		}
+
+		// =========================================================================
+		// Stack Outputs
+		// =========================================================================
+
 		ctx.Export("stravaTableName", stravaTable.Name)
 		ctx.Export("stravaTableArn", stravaTable.Arn)
 		ctx.Export("stravaNotificationsArn", stravaNotifications.Arn)
+		ctx.Export("audioInboundRawBucket", audioInboundRaw.Bucket)
+		ctx.Export("transcriptsStorageBucket", transcriptsStorage.Bucket)
 
 		return nil
 	})
